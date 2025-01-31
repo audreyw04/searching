06dad6ceb0e1005f505f0f28c08ecdb18858be3e
Audrey Weaver <auwe4400@colorado.edu>
2025-01-31 07:27:52 +0000

    Final commit: Adding all search files

--- README.md
+++ README.md
@@ -3,14 +3,16 @@
 Empirical and theoretic comparison of search methods.
 
 ## Usage
-### Emperical comparison
+### Emperical comparison Experiment 1
 ```
-usage: search.py [-h] --query_range QUERY_RANGE QUERY_RANGE QUERY_RANGE --database_size DATABASE_SIZE [--values_file VALUES_FILE] [--max_value MAX_VALUE] [--rounds ROUNDS] --out_file OUT_FILE
+usage: search.py [-h] --experiment_number NUMBER --query_percent PERCENT [--database_range DATABASE_RANGE DATABASE_RANGE DATABASE_RANGE] [--values_file VALUES_FILE] [--max_value MAX_VALUE] [--rounds ROUNDS] --out_file OUT_FILE 
+
 
 optional arguments:
   -h, --help            show this help message and exit
-  --query_range QUERY_RANGE QUERY_RANGE QUERY_RANGE
-                        Query size parameters (start stop step)
+  --database_range DATABASE_RANGE DATABASE_RANGE DATABASE_RANGE
+                        Database range (start, stop, step) 
+                            leave blank for experiment 2 (Default is 20,000
   --database_size DATABASE_SIZE
                         Database size
   --values_file VALUES_FILE
@@ -21,46 +23,40 @@ optional arguments:
   --out_file OUT_FILE   File to save plot to
 ```
 
-### Theoretic comparison
+## Example Experiment 1
 ```
-usage: sim.py [-h] --query_range QUERY_RANGE QUERY_RANGE QUERY_RANGE --database_size DATABASE_SIZE --out_file OUT_FILE [--width WIDTH] [--height HEIGHT]
+$ python src/search.py \
+    --experiment_number 1 \
+    --query_percent 10 \
+    --database_range 100 20000 2000 \
+    --values_file ./data/words.txt.gz \
+    --rounds 30 \
+    --out_file ../doc/db100-20000-200qp10.png 
+Binary search Time min 407483.0333333333 max 187627198.76666668
+Merge search Time min 389542.73333333334 max 140508460.76666668
+Hash table search Time min 91742.0 max 45307579.5
+Binary search Memory min 8859.733333333334 max 1714252.0
+Merge search Memory min 12760.533333333333 max 2303483.2
+Hash table search Memory min 15688.0 max 2477612.0
+```
+<center><img src="/doc/db100-20000-200qp10.png" width="600"/></center>
 
-optional arguments:
-  -h, --help            show this help message and exit
-  --query_range QUERY_RANGE QUERY_RANGE QUERY_RANGE
-                        Query size parameters (start stop step)
-  --database_size DATABASE_SIZE
-                        Database size
-  --out_file OUT_FILE   Output file
-  --width WIDTH         Width of plot in inches (default: 3)
-  --height HEIGHT       Height of plot in inches (default: 3)
+## Example Experiment 2
+```
+$ python ./src/search.py \
+    --experiment_number 2 \
+    --query_percent 90 \
+    --values_file ./data/words.txt.gz \
+    --rounds 30 \
+    --out_file ../doc/db20000_qp90_r30.png
+Binary search Time min 7142864.066666666 max 22955407.7
+Merge search Time min 5832611.633333334 max 21315806.933333334
+Hash table search Time min 3101845.566666667 max 5463796.466666667
+Binary search Memory min 238070.13333333333 max 382042.93333333335
+Merge search Memory min 238070.13333333333 max 382042.93333333335
+Hash table search Memory min 1044840.0 max 1188812.0
 ```
 
-## Example
-```
-python src/search.py \
-    --query_range 100 200000 10000 \
-    --database_size 200000  \
-    --rounds 5 \
-    --values_file data/words.txt.gz \
-    --out_file doc/q100-10000_d200000_str.png
-Binary search Time min 49883191.8 max 185743625.2
-Merge search Time min 91795216.6 max 155866725.0
-Hash table search Time min 18268700.0 max 35677233.4
-Binary search Memory min 2397976.0 max 4564396.0
-Merge search Memory min 2397976.0 max 4564376.0
-Hash table search Memory min 17329544.0 max 18849516.0
-```
-<center><img src="/doc/q100-10000_d200000_str.png" width="600"/></center>
-
-```
-python sim.py \
-    --query_range 100 200000 10000 \
-    --database_size 200000  \
-    --width 5 \
-    --height 4 \
-    --out_file ../doc/q100-10000_d200000_sim.png 
-```
 <center><img src="/doc/q100-10000_d200000_sim.png" width="600"/></center>
 
 
--- src/search.py
+++ src/search.py
@@ -9,15 +9,21 @@ import tracemalloc
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--query_range',
+    parser.add_argument('--experiment_number',
                         type=int,
                         required=True,
+                        help='Experiment 1: fixed query ratio and incrimental database size'
+                            + 'Experiment 2: fixed database size (for experiment 2, leave database blank')
+    parser.add_argument('--query_percent',
+                        type=float,
+                        required=True,
+                        help='Query size as percent of database size (as decimal)'
+                            + 'For Experiment 2: this is max percentage (10 incriments to 0)')
+    parser.add_argument('--database_range',
+                        type=int,
                         nargs=3,
-                        help='Query size parameters (start stop step)')
-    parser.add_argument('--database_size',
-                        type=int,
-                        required=True,
-                        help='Database size')
+                        help='Database range (start, stop, step) '
+                            + 'leave blank for experiment 2 (Default is 20,000')
     parser.add_argument('--values_file',
                         type=str,
                         help='A gziped text file containing values to ' \
@@ -114,13 +120,76 @@ def get_db_queries(num_database, num_queries, max_value=None, V=None):
 
     return D, Q
 
-def main():
-    args = get_args()
+def plot_result(x_values, binary_search_mean_times, merge_search_mean_times, hash_table_search_mean_times, binary_search_mean_mems, merge_search_mean_mems, hash_table_search_mean_mems, args):
+    fig, axs = plt.subplots(2,1, figsize=(args.width, args.height))
 
-    V = None
-    if args.values_file:
-        V = read_values_file(args.values_file)
+    ax = axs[0]
+    ax.plot(x_values,
+            binary_search_mean_times,
+            label='Binary search')
+    print('Binary search',
+          'Time',
+          'min', min(binary_search_mean_times),
+          'max', max(binary_search_mean_times))
 
+    ax.plot(x_values,
+            merge_search_mean_times,
+            label='Merge search')
+    print('Merge search',
+          'Time',
+          'min', min(merge_search_mean_times),
+          'max', max(merge_search_mean_times))
+
+    ax.plot(x_values,
+            hash_table_search_mean_times,
+            label='Hash table search')
+    print('Hash table search',
+          'Time',
+          'min', min(hash_table_search_mean_times),
+          'max', max(hash_table_search_mean_times))
+
+    ax.set(ylabel='Time (ns)',
+           title=f'Search algorithms (database size: {args.database_range})')
+    ax.set_xticklabels([])
+    ax.legend(loc='best', frameon=False, ncol=3)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    ax = axs[1]
+    ax.plot(x_values,
+            binary_search_mean_mems,
+            label='Binary search')
+    print('Binary search',
+          'Memory',
+          'min', min(binary_search_mean_mems),
+          'max', max(binary_search_mean_mems))
+    ax.plot(x_values,
+            merge_search_mean_mems,
+            label='Merge search')
+
+    print('Merge search',
+          'Memory',
+          'min', min(merge_search_mean_mems),
+          'max', max(merge_search_mean_mems))
+
+    ax.plot(x_values,
+            hash_table_search_mean_mems,
+            label='Hash table search')
+    print('Hash table search',
+          'Memory',
+          'min', min(hash_table_search_mean_mems),
+          'max', max(hash_table_search_mean_mems))
+
+    ax.set(xlabel='Query size',
+           ylabel='Memory (bytes)')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    fig.tight_layout()
+    fig.savefig(args.out_file)
+
+def experiment_1(args, V):
+    qr = args.query_percent
 
     binary_search_mean_times = []
     merge_search_mean_times = []
@@ -130,9 +199,9 @@ def main():
     merge_search_mean_mems = []
     hash_table_search_mean_mems = []
 
-    for q_size in range(args.query_range[0],
-                        args.query_range[1],
-                        args.query_range[2]):
+    database_sizes = range(args.database_range[0], args.database_range[1], args.database_range[2])
+
+    for db_size in database_sizes:
 
         binary_search_times = []
         merge_search_times = []
@@ -143,12 +212,7 @@ def main():
         hash_table_search_mems = []
 
         for _ in range(args.rounds):
-            D, Q = get_db_queries(args.database_size,
-                                  q_size,
-                                  args.max_value,
-                                  V=V)
-
-            
+            D, Q = get_db_queries(db_size, int(db_size * qr), args.max_value, V=V)
 
             start = time.monotonic_ns()
             ss_found = binary_search(Q.copy(), D.copy())
@@ -175,10 +239,10 @@ def main():
             tracemalloc.stop()
 
 
-            start = time.monotonic_ns()
+            ht_start = time.monotonic_ns()
             ht_found = hash_table(Q.copy(), D.copy())
-            stop = time.monotonic_ns()
-            hash_table_search_times.append(stop - start)
+            ht_stop = time.monotonic_ns()
+            hash_table_search_times.append(ht_stop - ht_start)
 
             tracemalloc.start()
             ht_found = hash_table(Q.copy(), D.copy())
@@ -197,87 +261,98 @@ def main():
         merge_search_mean_mems.append(np.mean(merge_search_mems))
         hash_table_search_mean_mems.append(np.mean(hash_table_search_mems))
 
-    fig, axs = plt.subplots(2,1, figsize=(args.width, args.height))
-
-    ax = axs[0]
-    ax.plot(range(args.query_range[0],
-                   args.query_range[1],
-                   args.query_range[2]),
-            binary_search_mean_times,
-            label='Binary search')
-    print('Binary search',
-          'Time',
-          'min', min(binary_search_mean_times),
-          'max', max(binary_search_mean_times))
-
-    ax.plot(range(args.query_range[0],
-                   args.query_range[1],
-                   args.query_range[2]),
-            merge_search_mean_times,
-            label='Merge search')
-    print('Merge search',
-          'Time',
-          'min', min(merge_search_mean_times),
-          'max', max(merge_search_mean_times))
-
-    ax.plot(range(args.query_range[0],
-                   args.query_range[1],
-                   args.query_range[2]),
-            hash_table_search_mean_times,
-            label='Hash table search')
-    print('Hash table search',
-          'Time',
-          'min', min(hash_table_search_mean_times),
-          'max', max(hash_table_search_mean_times))
-
-    ax.set(ylabel='Time (ns)',
-           title=f'Search algorithms (database size: {args.database_size})')
-    ax.set_xticklabels([])
-    ax.legend(loc='best', frameon=False, ncol=3)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-
-    ax = axs[1]
-    ax.plot(range(args.query_range[0],
-                   args.query_range[1],
-                   args.query_range[2]),
-            binary_search_mean_mems,
-            label='Binary search')
-    print('Binary search',
-          'Memory',
-          'min', min(binary_search_mean_mems),
-          'max', max(binary_search_mean_mems))
-    ax.plot(range(args.query_range[0],
-                   args.query_range[1],
-                   args.query_range[2]),
-            merge_search_mean_mems,
-            label='Merge search')
-
-    print('Merge search',
-          'Memory',
-          'min', min(merge_search_mean_mems),
-          'max', max(merge_search_mean_mems))
-
-    ax.plot(range(args.query_range[0],
-                   args.query_range[1],
-                   args.query_range[2]),
-            hash_table_search_mean_mems,
-            label='Hash table search')
-    print('Hash table search',
-          'Memory',
-          'min', min(hash_table_search_mean_mems),
-          'max', max(hash_table_search_mean_mems))
-
-    ax.set(xlabel='Query size',
-           ylabel='Memory (bytes)')
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
+    plot_result(database_sizes, binary_search_mean_times, merge_search_mean_times, hash_table_search_mean_times, binary_search_mean_mems, merge_search_mean_mems, hash_table_search_mean_mems, args)
 
 
+def experiment_2(args, V):
+    database_size = 20000
+    qp = args.query_percent /100
+    query_sizes = [int(database_size * (qp * i / 10)) for i in range(11)]
+
+    binary_search_mean_times = []
+    merge_search_mean_times = []
+    hash_table_search_mean_times = []
+
+    binary_search_mean_mems = []
+    merge_search_mean_mems = []
+    hash_table_search_mean_mems = []
+
+    for query_size in query_sizes:
+        binary_search_times = []
+        merge_search_times = []
+        hash_table_search_times = []
+
+        binary_search_mems = []
+        merge_search_mems = []
+        hash_table_search_mems = []
+
+        for _ in range(args.rounds):
+            D, Q = get_db_queries(database_size, query_size, args.max_value, V=V)
+
+            start = time.monotonic_ns()
+            ss_found = binary_search(Q.copy(), D.copy())
+            stop = time.monotonic_ns()
+            binary_search_times.append(stop - start)
 
 
-    fig.tight_layout()
-    fig.savefig(args.out_file)
+            tracemalloc.start()
+            ss_found = binary_search(Q.copy(), D.copy())
+            mem = tracemalloc.get_traced_memory()
+            binary_search_mems.append(mem[1] - mem[0])
+            tracemalloc.stop()
+
+
+            start = time.monotonic_ns()
+            ms_found = merge_search(Q.copy(), D.copy())
+            stop = time.monotonic_ns()
+            merge_search_times.append(stop - start)
+
+            tracemalloc.start()
+            ms_found = merge_search(Q.copy(), D.copy())
+            mem = tracemalloc.get_traced_memory()
+            merge_search_mems.append(mem[1] - mem[0])
+            tracemalloc.stop()
+
+
+            ht_start = time.monotonic_ns()
+            ht_found = hash_table(Q.copy(), D.copy())
+            ht_stop = time.monotonic_ns()
+            hash_table_search_times.append(ht_stop - ht_start)
+
+            tracemalloc.start()
+            ht_found = hash_table(Q.copy(), D.copy())
+            mem = tracemalloc.get_traced_memory()
+            hash_table_search_mems.append(mem[1] - mem[0])
+            tracemalloc.stop()
+
+            #assert(ss_found == ms_found)
+            #assert(ss_found == ht_found)
+
+        binary_search_mean_times.append(np.mean(binary_search_times))
+        merge_search_mean_times.append(np.mean(merge_search_times))
+        hash_table_search_mean_times.append(np.mean(hash_table_search_times))
+
+        binary_search_mean_mems.append(np.mean(binary_search_mems))
+        merge_search_mean_mems.append(np.mean(merge_search_mems))
+        hash_table_search_mean_mems.append(np.mean(hash_table_search_mems))
+
+    plot_result(query_sizes, binary_search_mean_times, merge_search_mean_times, hash_table_search_mean_times, binary_search_mean_mems, merge_search_mean_mems, hash_table_search_mean_mems, args)
+
+
+
+
+def main():
+    args = get_args()
+
+    V = None
+    if args.values_file:
+        V = read_values_file(args.values_file)
+
+    if args.experiment_number == 1:
+        experiment_1(args, V)
+    elif args.experiment_number == 2:
+        experiment_2(args, V)
+
 
 
     
